a5032ce5691acf74f322254357d85e18549c4563
Alexander Moerman <alexander.moerman@gmail.com>
2024-11-26 12:54:13 +0100

    Remove standard for a bit

--- src/DcmParse.StandardGenerator/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// Just parse this:
-// list of CIODs: https://github.com/innolitics/dicom-standard/blob/master/standard/ciods.json
-// list of modules per CIOD: https://github.com/innolitics/dicom-standard/blob/master/standard/ciod_to_modules.json
-// Etc
-// included here as standard.zip
-
-Console.WriteLine("Hello world");
